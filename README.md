0de72a3d068345d6d5ac155f079c261ec64f6504
Hyungwon Lee <hueleev@gmail.com>
2022-05-10 15:18:12 +0900

    end

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Vanila Redux
+# [Vanila Redux](https://nomadcoders.co/redux-for-beginners)
 
 Learning Vanila-Redux and React-Redux
---
+++ src/routes/ToDo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { remove } from '../store';
+
+function ToDo({ text, id, onBtnClick }) {
+	return (
+		<li>
+			<Link to={`/${id}`}>
+				{id} - {text}
+			</Link>
+			{` 🎈 `}
+			<button onClick={onBtnClick}>DEL</button>
+		</li>
+	);
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+	return {
+		onBtnClick: () => dispatch(remove(ownProps.id)),
+	};
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
